c6388d7401c2e91d760cd83faecedbd330bafa1d
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2025-05-11 23:50:10 +0530

    wefwe

--- routes/location.routes.js
+++ routes/location.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/location.controller');
 router.post('/', ctrl.create);
-router.get('/:id', ctrl.getByEmployee);
+router.get('/get-location', ctrl.getByEmployee);
 module.exports = router;
