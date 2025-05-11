2cd2640762f85f9824d03cd9ddc4bd47a9edf0d7
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2025-05-11 23:16:12 +0530

    location

--- controllers/location.controller.js
+++ controllers/location.controller.js
@@ -1,19 +1,82 @@
+// const Location = require('../models/location.model');
+
+// exports.create = async (req, res) => {
+//   try {
+//     const data = await Location.create(req.body);
+//     res.send({ statusCode: 200, success: true, message: 'Location saved', result: data });
+//   } catch (err) {
+//     res.send({ statusCode: 500, success: false, message: 'Error saving location', result: err });
+//   }
+// };
+
+// exports.getByEmployee = async (req, res) => {
+//   try {
+//     const data = await Location.find({ employeeId: req.params.id }).sort({ timestamp: -1 }).limit(1);
+//     res.send({ statusCode: 200, success: true, message: 'Latest location', result: data[0] });
+//   } catch (err) {
+//     res.send({ statusCode: 500, success: false, message: 'Error fetching location', result: err });
+//   }
+// };
+
+// controllers/location.controller.js
 const Location = require('../models/location.model');
 
 exports.create = async (req, res) => {
   try {
     const data = await Location.create(req.body);
-    res.send({ statusCode: 200, success: true, message: 'Location saved', result: data });
+    res.send({
+      statusCode: 200,
+      success: true,
+      message: 'Location saved',
+      result: {
+        _id: data._id,
+        employeeId: data.employeeId,
+        latitude: data.latitude,
+        longitude: data.longitude,
+        timestamp: data.timestamp
+      }
+    });
   } catch (err) {
-    res.send({ statusCode: 500, success: false, message: 'Error saving location', result: err });
+    res.send({
+      statusCode: 500,
+      success: false,
+      message: 'Error saving location',
+      result: err
+    });
   }
 };
 
 exports.getByEmployee = async (req, res) => {
   try {
-    const data = await Location.find({ employeeId: req.params.id }).sort({ timestamp: -1 }).limit(1);
-    res.send({ statusCode: 200, success: true, message: 'Latest location', result: data[0] });
+    const data = await Location.find({ employeeId: req.params.id })
+      .sort({ timestamp: -1 })
+      .limit(1);
+    if (data.length === 0) {
+      return res.send({
+        statusCode: 404,
+        success: false,
+        message: 'No location found',
+        result: null
+      });
+    }
+    res.send({
+      statusCode: 200,
+      success: true,
+      message: 'Latest location',
+      result: {
+        _id: data[0]._id,
+        employeeId: data[0].employeeId,
+        latitude: data[0].latitude,
+        longitude: data[0].longitude,
+        timestamp: data[0].timestamp
+      }
+    });
   } catch (err) {
-    res.send({ statusCode: 500, success: false, message: 'Error fetching location', result: err });
+    res.send({
+      statusCode: 500,
+      success: false,
+      message: 'Error fetching location',
+      result: err
+    });
   }
 };
